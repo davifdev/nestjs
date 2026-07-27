05b16c8eb756e047925505e22d9b1affba897e99
Davi Fernandes <davi.fer159@gmail.com>
2026-07-27 13:53:51 -0300

    chore: core concepts of modules controllers and providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConceptsManualModule } from '../concepts-manual/concepts-manual.module';
 
 @Module({
-  imports: [],
+  imports: [ConceptsManualModule],
   controllers: [AppController],
   providers: [AppService],
 })

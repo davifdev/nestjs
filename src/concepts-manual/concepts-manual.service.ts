7c6490e8f9da9f886ce7160d33a71c0174d2c8cf
Davi Fernandes <davi.fer159@gmail.com>
2026-07-29 12:16:25 -0300

    initial crud

--- src/concepts-manual/concepts-manual.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class ConceptsManualService {
-  exampleServiceMethod(): string {
-    return 'service creation manual example';
-  }
-}

c9b6ef3f72d90d39557a76d2d1dd6b6e02c14a41
Davi Fernandes <davi.fer159@gmail.com>
2026-08-18 19:55:49 -0300

    feat: add enviroment variables

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,45 +1,27 @@
-import {
-  MiddlewareConsumer,
-  Module,
-  NestModule,
-  RequestMethod,
-} from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MessageModule } from '../message/message.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PersonsModule } from '../persons/persons.module';
-import { SimpleMiddleware } from '../common/middlewares/simple.middleware';
-import { APP_FILTER, APP_GUARD } from '@nestjs/core';
-import { MyExceptionFilter } from '../common/filters/my-exception.filter';
-import { IsAdminGuard } from '../common/guards/is-admin.guard';
+import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'password',
-      database: 'nestjs_db',
-      autoLoadEntities: true, // Carrega entidades sem precisar especifica-las
-      synchronize: true, // Sincroniza o banco de dados com as entidades (não recomendado para produção)
+      type: process.env.DATABASE_TYPE as 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_DATABASE,
+      autoLoadEntities: Boolean(process.env.DATABASE_AUTO_LOAD_ENTITIES), // Carrega entidades sem precisar especifica-las
+      synchronize: Boolean(process.env.DATABASE_SYNCHRONIZE), // Sincroniza o banco de dados com as entidades (não recomendado para produção)
     }),
     MessageModule,
     PersonsModule,
   ],
   controllers: [AppController],
-  providers: [
-    AppService,
-    { provide: APP_FILTER, useClass: MyExceptionFilter },
-    { provide: APP_GUARD, useClass: IsAdminGuard },
-  ],
+  providers: [AppService],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(SimpleMiddleware).forRoutes({
-      path: '*',
-      method: RequestMethod.ALL,
-    });
-  }
-}
+export class AppModule {}
--- src/message/message.module.ts
+++ src/message/message.module.ts
@@ -4,16 +4,10 @@ import { MessageService } from './message.service';
 import { Message } from './entities/message.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PersonsModule } from '../persons/persons.module';
-import { MyDynamicModule } from '../my-dynamic/my-dynamic.module';
-
 @Module({
   imports: [
     TypeOrmModule.forFeature([Message]),
     forwardRef(() => PersonsModule),
-    MyDynamicModule.register({
-      apiKey: 'minha_api_key',
-      apiUrl: 'http://localhost:3000',
-    }),
   ],
   controllers: [MessageController],
   providers: [MessageService],

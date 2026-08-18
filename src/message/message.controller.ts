8758cce440ca15673eb5ce8096f0b6fc0eb384c6
Davi Fernandes <davi.fer159@gmail.com>
2026-08-18 18:56:23 -0300

    feat: created dynamic modules

--- src/message/message.controller.ts
+++ src/message/message.controller.ts
@@ -1,58 +1,30 @@
 import {
-  BadRequestException,
   Body,
   Controller,
   Delete,
   Get,
   HttpCode,
   HttpStatus,
-  Inject,
   Param,
   Patch,
   Post,
   Query,
   UseInterceptors,
-  UsePipes,
 } from '@nestjs/common';
 import { MessageService } from './message.service';
 import { CreateMessageDto } from './dto/create-message-dto';
 import { UpdateMessageDto } from './dto/update-message.dto';
 import { PaginationDto } from '../common/dto/pagination.dto';
-import { ParseIntIdPipe } from '../common/pipes/parse-int-id.pipe';
+
 import { AddHeaderInterceptor } from '../common/interceptors/add-header.interceptor';
 import { TimingConnectionInterceptor } from '../common/interceptors/time-connection.interceptor';
 import { ErrorHandlingInterceptor } from '../common/interceptors/error-handling.interceptor';
-import { SimpleCacheInterceptor } from '../common/interceptors/simple-cache.interceptor';
-import { ChangeDataInterceptor } from '../common/interceptors/change-data.interceptor';
-import { AuthTokenInterceptor } from '../common/interceptors/auth-token.interceptor';
-import { ReqDataParam } from '../common/params/req-data-param.decorator';
-import { MessageUtils } from './message.utils';
 
-import { RegexProtocol } from '../common/regex/regex.protocol';
-import { SERVER_NAME } from './constants/server-name.constant';
-import {
-  ONLY_LOWERCASE_LETTERS_REGEX,
-  REMOVE_SPACES_REGEX,
-} from './constants/message.constant';
-import { OnlyLowercaseLettersRegex } from '../common/regex/only-lowercase-letters';
+import { ReqDataParam } from '../common/params/req-data-param.decorator';
+
 @Controller('message')
-// @UsePipes(ParseIntIdPipe)
-// @UseInterceptors(
-//   SimpleCacheInterceptor,
-//   ChangeDataInterceptor,
-//   AuthTokenInterceptor,
-// )
 export class MessageController {
-  constructor(
-    private readonly messageService: MessageService,
-    private readonly messageUtils: MessageUtils,
-    @Inject(SERVER_NAME)
-    private readonly serverName: string,
-    @Inject(REMOVE_SPACES_REGEX)
-    private readonly removeSpacesRegex: RegexProtocol,
-    @Inject(ONLY_LOWERCASE_LETTERS_REGEX)
-    private readonly onlyLowerCaseRegex: OnlyLowercaseLettersRegex,
-  ) {}
+  constructor(private readonly messageService: MessageService) {}
 
   @UseInterceptors(TimingConnectionInterceptor, ErrorHandlingInterceptor)
   @HttpCode(HttpStatus.OK)
@@ -62,7 +34,7 @@ export class MessageController {
     @ReqDataParam('method') method,
   ) {
     console.log(method);
-    throw new BadRequestException();
+
     return this.messageService.findAll(paginationDto);
   }
 
@@ -70,9 +42,6 @@ export class MessageController {
   @HttpCode(HttpStatus.OK)
   @Get(':messageId')
   findOne(@Param('messageId') messageId: number) {
-    console.log(this.removeSpacesRegex.execute(this.serverName));
-    console.log(this.onlyLowerCaseRegex.execute(this.serverName));
-
     return this.messageService.findOne(messageId);
   }
 
--- src/message/message.module.ts
+++ src/message/message.module.ts
@@ -1,46 +1,22 @@
-/* eslint-disable no-constant-condition */
 import { forwardRef, Module } from '@nestjs/common';
 import { MessageController } from './message.controller';
 import { MessageService } from './message.service';
 import { Message } from './entities/message.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PersonsModule } from '../persons/persons.module';
-import { MessageUtils } from './message.utils';
-import { RemoveSpacesRegex } from '../common/regex/remove-spaces.regex';
-import { OnlyLowercaseLettersRegex } from '../common/regex/only-lowercase-letters';
-import { SERVER_NAME } from './constants/server-name.constant';
-import {
-  ONLY_LOWERCASE_LETTERS_REGEX,
-  REMOVE_SPACES_REGEX,
-} from './constants/message.constant';
-import { RegexFactory } from '../common/regex/regex.factory';
+import { MyDynamicModule } from '../my-dynamic/my-dynamic.module';
+
 @Module({
   imports: [
     TypeOrmModule.forFeature([Message]),
     forwardRef(() => PersonsModule),
+    MyDynamicModule.register({
+      apiKey: 'minha_api_key',
+      apiUrl: 'http://localhost:3000',
+    }),
   ],
   controllers: [MessageController],
-  providers: [
-    MessageService,
-    RegexFactory,
-    { provide: MessageUtils, useClass: MessageUtils },
-    { provide: SERVER_NAME, useValue: 'My Name Is Davi!' },
-    {
-      provide: ONLY_LOWERCASE_LETTERS_REGEX,
-      useClass: OnlyLowercaseLettersRegex,
-    },
-    {
-      provide: REMOVE_SPACES_REGEX,
-      useClass: RemoveSpacesRegex,
-    },
-    {
-      provide: REMOVE_SPACES_REGEX,
-      useFactory: (regexFactory: RegexFactory) => {
-        return regexFactory.create('RemoveSpacesRegex');
-      },
-      inject: [RegexFactory],
-    },
-  ],
+  providers: [MessageService],
   exports: [MessageService],
 })
 export class MessageModule {}
